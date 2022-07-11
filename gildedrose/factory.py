358afc46e334d5801ec1150733cc6a4c89216b8b
Scott O'Mary <omaryscott@gmail.com>
2022-07-11 09:48:44 -0500

    __repr__ and update function not needed for base class.

--- gildedrose/factory.py
+++ gildedrose/factory.py
@@ -4,17 +4,6 @@ class Item(object):
         self.quality = quality
         self.sell_in = sell_in
 
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
-
-    def update_quality(self):
-        if 50 > self.quality > 0:
-            if self.sell_in <= 0:
-                self.quality = self.quality - 2
-            else:
-                self.quality = self.quality - 1
-        self.sell_in = self.sell_in - 1
-
 
 class Factory(object):
     def create(self, name, sell_in, quality):
