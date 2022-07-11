fc25110c0702d97ba5da2d6fde9a65d3b0ff374f
Scott O'Mary <omaryscott@gmail.com>
2022-07-11 09:33:42 -0500

    Moved Item class to it's own file for consistency.

--- gildedrose/factory.py
+++ gildedrose/factory.py
@@ -1,4 +1,7 @@
-class ItemFactory(object):
+from item import Item
+
+
+class Factory(object):
     def create(self, name, sell_in, quality):
         if name == "Aged Brie":
             return AgedBrie(name, sell_in, quality)
@@ -10,24 +13,6 @@ class ItemFactory(object):
             return Conjured(name, sell_in, quality)
         else:
             return Item(name, sell_in, quality)
-
-
-class Item(object):
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.quality = quality
-        self.sell_in = sell_in
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
-
-    def update_quality(self):
-        if 50 > self.quality > 0:
-            if self.sell_in <= 0:
-                self.quality = self.quality - 2
-            else:
-                self.quality = self.quality - 1
-        self.sell_in = self.sell_in - 1
 
 
 class AgedBrie(Item):
